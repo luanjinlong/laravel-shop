8a29caa1824c5f4b3fd0efd85fbc0c9594ab7b42
xiaolong <646448149@qq.com>
2019-06-02 16:25:36 +0800

    stop

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -98,7 +98,6 @@ class OrdersController extends Controller
         // 只展示已支付的订单，并且默认按支付时间倒序排序
 //        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at', 'desc');
         $grid->model()->orderBy('id', 'desc');
-
         $grid->no('订单流水号');
         // 展示关联关系的字段时，使用 column 方法
         $grid->column('user.name', '买家');
@@ -107,9 +106,9 @@ class OrdersController extends Controller
         $grid->ship_status('物流')->display(function($value) {
             return Order::$shipStatusMap[$value];
         });
-        $grid->refund_status('退款状态')->display(function($value) {
-            return Order::$refundStatusMap[$value];
-        });
+//        $grid->refund_status('退款状态')->display(function($value) {
+//            return Order::$refundStatusMap[$value];
+//        });
         // 禁用创建按钮，后台不需要创建订单
         $grid->disableCreateButton();
         $grid->actions(function ($actions) {
